340c1891dad2b49b1c64ecd0bd0c3d41476ad2c2
IronBeardX <alejandroyeroval@gmail.com>
2023-01-02 17:32:47 -0500

    simulation flow in module

--- src/evo_sim.py
+++ src/evo_sim.py
@@ -3,15 +3,81 @@ from .evo_entity import *
 from .utils import *
 
 
-class EvoSim:  # TODO: How should we define starting Entities in the map
-    def __init__(self, world: EvoWorld, starting_states: dict[str, dict[str, any]]) -> None:
+class EvoSim:
+    def __init__(self,
+                 height,
+                 width,
+                 terrain_types,
+                 terrain_dist,
+                 finite=False,
+                 episodes_total=10,
+                 max_rounds_per_episode=1000,
+                 stop_condition=None
+                 ):
+        self.init_world(height, width, terrain_types, terrain_dist, finite)
+        self.entities_gen = []
+        self.entities = {}
+        self.intelligent_entities = {}
+        self.episodes_total = episodes_total
+        self.max_rounds = max_rounds_per_episode
+        self.stop_condition = stop_condition
+
+    def run(self, gen_world_pos):
+        for episode in range(self.episodes_total):
+            self.world = self.world_gen()
+            for entity_gen_position, world_position in gen_world_pos:
+                self.instantiate_entity(entity_gen_position, world_position)
+            self.run_episode()
+            self.entities = {}
+            self.intelligent_entities = {}
+
+    # Implement statistics
+    def run_episode(self):
+        for round in range(self.max_rounds):
+            # Time comes for us all ...
+            for entity_id in self.intelligent_entities:
+                entity = self.intelligent_entities[entity_id]
+                entity.pass_time()
+            for entity_id in self.entities:
+                entity = self.entities[entity_id]
+                entity.pass_time()
+
+            # Checking stop condition if defined
+            if self.stop_condition is not None:
+                if self.stop_condition(self):
+                    return
+
+            # Executing entities actions
+            for entity_id in self.entities:
+                entity = self.entities[entity_id]
+
+                # Executing perception actions:
+                perception_dict = {}
+                for action in entity.perception_actions:
+                    perception_dict[action.id] = self.world.execute_action(
+                        action)
+                # The entity executes its action based on its world perception,
+                # which returns world and simulation actions to be executed
+                world_actions, sim_actions = entity.act(perception_dict)
+                for action in world_actions:
+                    self.world.execute_action(action)
+                for action in sim_actions:
+                    self.execute_action(action)
+
+    def execute_action(self, action):
         pass
 
-    def run():
-        pass
+    def init_world(self, height, width, terrain_types, terrain_dist, finite):
+        self.world_gen = lambda: EvoWorld(height, width, terrain_types, terrain_dist, finite)
 
-    def run_episode():
-        pass
+    def add_entity_gen(self, entity_instance_gen):
+        self.entities_gen.append(entity_instance_gen)
 
-    def gen_starting_states():
-        pass
+    def instantiate_entity(self, entity_gen_position, world_position):
+        entity = self.entities_gen[entity_gen_position]()
+        if entity.is_intelligent:
+            self.intelligent_entities[entity.id] = entity
+        else:
+            self.entities[entity.id] = entity
+        self.world.place_entity(entity.id, world_position,
+                                entity.rep, entity.coexistence)
