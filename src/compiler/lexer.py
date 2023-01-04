c8d20396cbfcde487873c284fd64b6f8710dd463
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-01-04 08:16:52 -0500

    add 'newline' token rule

--- src/compiler/lexer.py
+++ src/compiler/lexer.py
@@ -3,12 +3,18 @@ import src.compiler.ply.lex as lex
 
 literals = '+-*/%()'
 
-tokens = ('INTDIV', 'NUMBER')
+tokens = ('INTDIV', 'NUMBER', 'newline')
 
 def get_lexer(*args, **kwargs):
     # token rules
     t_INTDIV = r'//'
     t_NUMBER = r'-?(0|[1-9][0-9]*)(\.[0-9]*)?'
+
+    # match newline chars and record line count
+    def t_newline(t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
+        return t
 
     # ignore tabs and spaces
     t_ignore = r' \t'
