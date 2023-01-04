6cb525f80140262bff9c7ae48fbb6bed662314b1
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-01-04 15:38:58 -0500

    add ast nodes
    - ValueNode to evaluate literals
    - UnaryOpNode to apply a operation to one node
    - BinaryOpNode to apply a operation of two nodes

---
+++ src/compiler/ast.py
@@ -0,0 +1,33 @@
+from src.compiler.context import Context
+
+
+class Node:
+    def evaluate(self, context: Context):
+        raise NotImplementedError()
+
+class ValueNode(Node):
+    def __init__(self, value):
+        self.value = value
+    
+    def evaluate(self, context: Context):
+        return self.value
+
+class UnaryOpNode(Node):
+    def __init__(self, node, apply):
+        self.node = node
+        self.apply = apply
+    
+    def evaluate(self, context):
+        value = self.node.evaluate(context)
+        return self.apply(value)
+
+class BinaryOpNode(Node):
+    def __init__(self, leftnode, rightnode, apply):
+        self.leftnode = leftnode
+        self.rightnode = rightnode
+        self.apply = apply
+    
+    def evaluate(self, context):
+        left = self.leftnode.evaluate(context)
+        right = self.rightnode.evaluate(context)
+        return self.apply(left, right)
--- src/compiler/parser.py
+++ src/compiler/parser.py
@@ -1,7 +1,10 @@
+from math import pow
+
 import src.compiler.ply.yacc as yacc
 from src.compiler.lexer import tokens
 from src.compiler.util import parse_number, nth_root
-from math import pow
+from src.compiler.ast import ValueNode, UnaryOpNode, BinaryOpNode
+
 
 
 def get_parser(*args, **kwargs):
@@ -18,7 +21,7 @@ def get_parser(*args, **kwargs):
     # boolean expr productions
     def p_disjunction(p):
         "disjunction : conjunction OR conjunction"
-        p[0] = p[1] or p[3]
+        p[0] = BinaryOpNode(p[1], p[3], lambda x,y: x or y)
     
     def p_disjunction_conjunction(p):
         "disjunction : conjunction"
@@ -26,7 +29,7 @@ def get_parser(*args, **kwargs):
     
     def p_conjunction(p):
         "conjunction : negation AND negation"
-        p[0] = p[1] and p[3]
+        p[0] = BinaryOpNode(p[1], p[3], lambda x,y: x and y)
     
     def p_conjunction_negation(p):
         "conjunction : negation"
@@ -34,7 +37,7 @@ def get_parser(*args, **kwargs):
     
     def p_negation(p):
         "negation : NOT comparison"
-        p[0] = not p[2]
+        p[0] = UnaryOpNode(p[2], lambda x: not x)
     
     def p_negation_comparison(p):
         "negation : comparison"
@@ -43,27 +46,27 @@ def get_parser(*args, **kwargs):
     # comparison expr productions
     def p_comparison_eq(p):
         "comparison : expr EQ expr"
-        p[0] = p[1] == p[3]
+        p[0] = BinaryOpNode(p[1], p[3], lambda x,y: x == y)
     
     def p_comparison_neq(p):
         "comparison : expr NEQ expr"
-        p[0] = p[1] != p[3]
+        p[0] = BinaryOpNode(p[1], p[3], lambda x,y: x != y)
     
     def p_comparison_ge(p):
         "comparison : expr GE expr"
-        p[0] = p[1] >= p[3]
+        p[0] = BinaryOpNode(p[1], p[3], lambda x,y: x >= y)
     
     def p_comparison_le(p):
         "comparison : expr LE expr"
-        p[0] = p[1] <= p[3]
+        p[0] = BinaryOpNode(p[1], p[3], lambda x,y: x <= y)
     
     def p_comparison_gt(p):
         "comparison : expr '>' expr"
-        p[0] = p[1] > p[3]
+        p[0] = BinaryOpNode(p[1], p[3], lambda x,y: x > y)
     
     def p_comparison_lt(p):
         "comparison : expr '<' expr"
-        p[0] = p[1] < p[3]
+        p[0] = BinaryOpNode(p[1], p[3], lambda x,y: x < y)
     
     def p_comparison_expr(p):
         "comparison : expr"
@@ -72,11 +75,11 @@ def get_parser(*args, **kwargs):
     # expr productions
     def p_expr_sum(p):
         "expr : expr '+' term"
-        p[0] = p[1] + p[3]
+        p[0] = BinaryOpNode(p[1], p[3], lambda x,y: x + y)
     
     def p_expr_substr(p):
         "expr : expr '-' term"
-        p[0] = p[1] - p[3]
+        p[0] = BinaryOpNode(p[1], p[3], lambda x,y: x - y)
     
     def p_expr_term(p):
         "expr : term"
@@ -84,19 +87,19 @@ def get_parser(*args, **kwargs):
     
     def p_term_mult(p):
         "term : term '*' factor"
-        p[0] = p[1] * p[3]
+        p[0] = BinaryOpNode(p[1], p[3], lambda x,y: x * y)
     
     def p_term_div(p):
         "term : term '/' factor"
-        p[0] = p[1] / p[3]
+        p[0] = BinaryOpNode(p[1], p[3], lambda x,y: x / y)
     
     def p_term_rem(p):
         "term : term '%' factor"
-        p[0] = p[1] % p[3]
+        p[0] = BinaryOpNode(p[1], p[3], lambda x,y: x % y)
     
     def p_term_intdiv(p):
         "term : term INTDIV factor"
-        p[0] = p[1] // p[3]
+        p[0] = BinaryOpNode(p[1], p[3], lambda x,y: x // y)
     
     def p_term_factor(p):
         "term : factor"
@@ -104,7 +107,7 @@ def get_parser(*args, **kwargs):
     
     def p_factor_negative(p):
         "factor : '-' factor"
-        p[0] = -1 * p[2]
+        p[0] = UnaryOpNode(p[2], lambda x: -1 * x)
     
     def p_factor_power(p):
         "factor : power"
@@ -112,11 +115,11 @@ def get_parser(*args, **kwargs):
     
     def p_power_power(p):
         "power : atom '^' factor"
-        p[0] = pow(p[1], p[3])
+        p[0] = BinaryOpNode(p[1], p[3], lambda x,y: pow(x, y))
     
     def p_power_root(p):
         "power : atom '@' factor"
-        p[0] = nth_root(p[1], p[3])
+        p[0] = BinaryOpNode(p[1], p[3], lambda x,y: nth_root(x, y))
     
     def p_power_atom(p):
         "power : atom"
@@ -124,15 +127,15 @@ def get_parser(*args, **kwargs):
     
     def p_atom_number(p):
         "atom : NUMBER"
-        p[0] = parse_number(p[1])
+        p[0] = ValueNode(parse_number(p[1]))
     
     def p_atom_false(p):
         "atom : FALSE"
-        p[0] = False
+        p[0] = ValueNode(False)
     
     def p_atom_true(p):
         "atom : TRUE"
-        p[0] = True
+        p[0] = ValueNode(True)
     
     def p_atom_group(p):
         "atom : '(' expr ')'"
