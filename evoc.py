edc3a4ac7f7b5d4fed9c4de55c9a4334f8e354d7
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-01-20 09:17:48 -0500

    finish evoc implementation

--- evoc.py
+++ evoc.py
@@ -1,5 +1,8 @@
 # importing required modules
 import argparse
+from src.compiler.lexer import get_lexer
+from src.compiler.parser import get_parser
+from src.compiler.context import Context
 
 def join_string_list(text_list):
     text = ''
@@ -13,13 +16,21 @@ parser = argparse.ArgumentParser(description="An evolution simulator")
 parser.add_argument("path", nargs=1, metavar="path",
                     type=str, help="The path of the evo script.")
 
+parser.add_argument('-d', '--debug', action="store_true", help='Enable debugging')
+
 # parse the arguments from standard input
 args = parser.parse_args()
 
 path = args.path[0]
 
+debug = args.debug
+lexer = get_lexer(debug=debug)
+parser = get_parser(debug=debug, start="program")
+context = Context(debug=debug)
+
 # Open file in path and read all text
 with open(path) as file:
-    text = join_string_list(file.readlines())
-    print(text)
+    code = join_string_list(file.readlines())
+    program = parser.parse(code, lexer = lexer)
+    program.evaluate(context)
 
