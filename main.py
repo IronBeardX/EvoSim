a5389e472ca577a01a1deddb0281d6e5c5170aac
IronBeardX <alejandroyeroval@gmail.com>
2023-01-24 01:27:59 -0500

    reworking main

--- main.py
+++ main.py
@@ -6,6 +6,45 @@ from src.utils import *
 from random import randint
 
 
+HEALTH = Health(min_val=0, max_val=4)
+HUNGER = Hunger(min_val=0, max_val=4)
+LEGS = Legs(min_val=0, max_val=4)
+EYE = Eye(min_val=0, max_val=4)
+ARMS = Arms(min_val=0, max_val=4)
+HORNS = Horns(min_val=0, max_val=4)
+SMELL = Smell(min_val=0, max_val=4)
+FINS = Fins(min_val=0, max_val=4)
+NOSE = Nose(min_val=0, max_val=4)
+MOUTH = Mouth(min_val=0, max_val=4)
+SMELLING = Smelling()
+VISIONRADIAL = VisionRadial()
+MOVE = Move()
+EAT = Eat()
+REPRODUCE = Reproduce()
+ATTACK = Attack()
+DEFEND = Defend()
+PICK = Pick()
+SWIMMING = Swimming()
+
+# Initializing Genetic Pool:
+POOL = GeneticPool()
+
+# Adding all genes to the genetic pool:
+POOL.add_genes([HEALTH, HUNGER, LEGS, EYE, ARMS, HORNS, SMELL, FINS, NOSE, MOUTH,
+               SMELLING, VISIONRADIAL, MOVE, EAT, REPRODUCE, ATTACK, DEFEND, PICK, SWIMMING])
+# Adding some edges:
+POOL.add_dependency("move", "legs")
+POOL.add_dependency("pick", "arms")
+POOL.add_dependency("attack", "arms")
+POOL.add_dependency("defend", "arms")
+POOL.add_dependency("attack", "horns")
+POOL.add_dependency("smelling", "nose")
+POOL.add_dependency("vision", "eye")
+POOL.add_dependency("swimming", "fins")
+
+
+
+
 def main():
     initial_dist = {}
     sim = EvoSim(10,
@@ -21,7 +60,7 @@ def main():
     sim.add_entity_gen(pickable_gen)
     sim.add_entity_gen(food_gen)
     sim.add_entity_gen(random_gen)
-    #generate entities generation list in random positions
+    # generate entities generation list in random positions
     positions_ent = gen_random_position_tuple_list(9, 9, 10)
     positions_food = gen_random_position_tuple_list(9, 9, 5)
     positions_pick = gen_random_position_tuple_list(9, 9, 5)
@@ -30,61 +69,10 @@ def main():
     gen_pos_ent.extend([(2, pos) for pos in positions_food])
     gen_pos_ent.extend([(1, pos) for pos in positions_pick])
 
-
     # clean terminal
     print(chr(27) + "[2J")
     sim.run(gen_pos_ent)
     print("Simulation Finished")
-
-
-def smeller_gen():
-    dna = gen_basic_dna_chain()
-    dna.extend(smeller_ext(dna))
-    return Organism(dna)
-
-
-def walker_gen():
-    dna = gen_basic_dna_chain()
-    dna.extend(watcher_ext(dna))
-    return Organism(dna)
-
-
-def random_gen():
-    dna = gen_basic_dna_chain()
-    #extending dna with all genes:
-    dna.extend(smeller_ext(dna))
-    dna.extend(watcher_ext(dna))
-    dna.extend(walker_ext(dna))
-    dna.extend(swimmer_ext(dna))
-    dna.extend(arms_ext(dna))
-    return RandomOrg(dna)
-
-def fully_capable_opportunistic_gen():
-    dna = gen_basic_dna_chain()
-    dna.extend(smeller_ext(dna))
-    dna.extend(watcher_ext(dna))
-    dna.extend(walker_ext(dna))
-    dna.extend(swimmer_ext(dna))
-    dna.extend(arms_ext(dna))
-    dna.extend(eater_ext(dna))
-    dna.append(Reproduce())
-    return OpportunisticOrg(dna, species = OpportunisticOrg)
-
-
-def gen_random_position_tuple_list(x, y, n):
-    #positions should be unique
-    positions = set()
-    while len(positions) < n:
-        positions.add((randint(0, x), randint(0, y)))
-    return list(positions)
-
-
-def food_gen():
-    return Food()
-
-
-def pickable_gen():
-    return PackableFood()
 
 
 if __name__ == "__main__":
